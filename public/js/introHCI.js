985c31691350c740e48b25505dc1c0d717bed7a5
Justin Phan <juphan@ucsd.edu>
2018-02-22 10:16:16 -0800

    lab7 done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$("button").click(callAnalytics);
+}
+
+function callAnalytics(e) {
+	ga("send", "event", "like", "click");
 }
